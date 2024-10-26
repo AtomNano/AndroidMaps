fb872a7f4c6bcf6c9337d8b673ed11804733d7a1
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2024-10-26 11:59:16 +0700

    v3

--- app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.myapplication.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -40,9 +39,30 @@ private lateinit var binding: ActivityMapsBinding
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        // Add a marker in Sydney and move the camera
+//        val padang = LatLng(-0.94924, 100.35427)
+//        mMap.addMarker(MarkerOptions().position(padang).title("Marker in Padang"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(padang))
+
+
+        // Set the map coordinates to Kyoto Japan.
+        val kyoto = LatLng(35.00116, 135.7681)
+
+        // Set the map type to Hybrid.
+        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        // Add a marker on the map coordinates.
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(kyoto)
+                .title("Kyoto")
+        )
+
+        // Move the camera to the map coordinates and zoom in closer.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+
+        // Display traffic.
+        googleMap.isTrafficEnabled = true
     }
 }
